e30c69f0c5b2368cc0ca98e651d1a7b5012c1990
Zandgall <Alexanderdgall@gmail.com>
2025-01-30 21:14:54 -0600

    Get and Update Profile

---
+++ getProfileData.php
@@ -0,0 +1,14 @@
+<?php
+include "main.php";
+
+$db = mainCheck();
+
+// Gather all messages from given chat
+$query = $db->prepare("SELECT * FROM `User` WHERE `Name` = ? AND `Password` = ?");
+$query->bind_param("ss", $_POST["username"], $_POST["password"]);
+
+// X Query failed
+checkpoint($query->execute(), "Database Query Failed", $query->error);
+
+// Return all chats from region
+echo json_encode(array("response"=>$query->get_result()->fetch_all(MYSQLI_ASSOC)[0], "error"=>false));
---
+++ updateProfile.php
@@ -0,0 +1,18 @@
+<?php
+
+include "main.php";
+
+$db = mainCheck();
+
+// X Wasn't provided an email
+checkpoint(isset($_POST["content"]), "Provide a message!");
+
+// Insert message
+$query = $db->prepare("UPDATE `User` SET `Name` = ?, `Password` = ?, `Email` = ?)");
+$query->bind_param("sss", $_POST["username"], $_POST["password"], $_POST["email"]);
+
+// X Query failed
+checkpoint($query->execute(), "Database Query Failed", $query->error);
+
+// Return true
+echo json_encode(array("response"=>true, "error"=>false, "message"=>"Account updated successfully!");
