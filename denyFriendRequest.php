2b2e97a71bd43e145cd2549f5763fc27d3ff596c
Zander Gall <alexanderdgall@gmail.com>
2025-02-16 15:30:24 -0600

    Deny Friend Request

---
+++ denyFriendRequest.php
@@ -0,0 +1,17 @@
+<?php
+include "main.php";
+
+$db = mainCheck();
+
+// X Wasn't provided a person to add as a friend
+checkpoint(isset($_POST["target"]), "Provide a target to deny as a friend!");
+
+// Insert message
+$query = $db->prepare("UPDATE `Friends` SET `RequestStatus`='DENIED' WHERE User2 = (SELECT UserID FROM User WHERE Name = ? AND Password = ?) AND User1 = (SELECT UserID FROM User WHERE Name = ?) AND `RequestStatus` = 'ACTIVE'");
+$query->bind_param("sss", $_POST["username"], $_POST["password"], $_POST["target"]);
+
+// X Query failed
+checkpoint($query->execute(), "Database Query Failed", $query->error);
+
+// Return all chats from region
+echo json_encode(array("response" => ($query->affected_rows > 0), "error" => false));
