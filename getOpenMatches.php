bca714b5f7c8ce96f1923cdfc1a0c8394d15aedb
Zander Gall <alexanderdgall@gmail.com>
2025-02-17 17:53:49 -0600

    getOpenMatches patch

--- getOpenMatches.php
+++ getOpenMatches.php
@@ -4,10 +4,10 @@ $db = mainCheck();
 
 // Gather all friends current user has
 $query = $db->prepare(
-	"SELECT m.MatchID, m.Chat, m.WhitePlayer, u1.Name as WhiteName, m.BlackPlayer, u2.Name as BlackName, m.Result FROM `ChessMatch` AS m
+	"SELECT m.MatchID, m.Chat, m.WhitePlayer, u1.Name as WhiteName, m.BlackPlayer, u2.Name as BlackName, m.Status FROM `ChessMatch` AS m
 	INNER JOIN `User` AS u1 ON (m.WhitePlayer = u1.UserID)
 	INNER JOIN `User` AS u2 ON (m.BlackPlayer = u2.UserID)
-	WHERE (u1.Name = ? OR u2.Name = ?) AND m.Result = \"InProgress\""
+	WHERE (u1.Name = ? OR u2.Name = ?) AND m.Status = \"InProgress\""
 );
 
 $query->bind_param("ss", $_POST["username"], $_POST["username"]);
